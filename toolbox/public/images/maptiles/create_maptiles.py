f63ddf3c4f61b788e88e21e47bbc5c38cf0b8702
komugico <shu.isaka.pc+develop@gmail.com>
2021-06-11 12:37:17 +0900

    #5 バグってるけどとりあえずアップする

--- toolbox/public/images/maptiles/create_maptiles.py
+++ toolbox/public/images/maptiles/create_maptiles.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import os
 
-SIZE = 4096
-img = Image.open("map.png").crop((w - 1024, h - 1024, w, h)).resize((SIZE, SIZE))
+SIZE = 2048
+img = Image.open("map.png").resize((SIZE, SIZE))
 
 for z in range(4):
     for x in range(2 ** z):
@@ -10,4 +10,4 @@ for z in range(4):
         for y in range(2 ** z):
             width = SIZE // 2 ** z
             height = SIZE // 2 ** z
-            img.crop((width * x, height * y, width * (x + 1), height * (y + 1))).save("./{0}/{1}/{2}.png".format(z, x, y))
+            img.crop((width * x, height * y, width * (x + 1), height * (y + 1))).resize((512, 512)).save("./{0}/{1}/{2}.png".format(z, x, y))
